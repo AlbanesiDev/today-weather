3cfc3c53772e9127c7595eeb43d7861a38e777ef
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-26 21:20:12 -0300

    Service that manages the language settings of the application.

---
+++ src/app/core/lang/lang.service.ts
@@ -0,0 +1,52 @@
+import { LocalStorageService } from './../services/local-storage.service';
+import { Injectable, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+/**
+ * A service that manages language settings for the application.
+ * It utilizes the LocalStorageService to persist user's language preference
+ * and the TranslateService to apply the language across the app.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class LangService {
+  /**
+   * Service responsible for managing the translation and localization.
+   */
+  private translateService: TranslateService = inject(TranslateService);
+
+  /**
+   * Service responsible for interacting with the browser's local storage.
+   */
+  private localStorageService: LocalStorageService =
+    inject(LocalStorageService);
+
+  /**
+   * The key used to store the language setting in local storage.
+   */
+  private langKeyStorage: string = 'userLangStorage';
+
+  /**
+   * Initializes the default language for the application.
+   * It checks the local storage for an existing language setting,
+   * and uses it if available. Otherwise, it defaults to English ('en').
+   */
+  public initializeLang() {
+    const langStorage = this.localStorageService.getItem(this.langKeyStorage);
+    if (langStorage !== null) {
+      this.translateService.setDefaultLang(langStorage);
+    } else {
+      this.translateService.setDefaultLang('en');
+    }
+  }
+
+  /**
+   * Sets the application's language and stores the preference in local storage.
+   * @param lang The language code to set as the current language.
+   */
+  public setLang(lang: string): void {
+    this.translateService.use(lang);
+    this.localStorageService.setItem(this.langKeyStorage, lang);
+  }
+}
