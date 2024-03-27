af9d0ac56ee93550e5e23328ca7bd040f3170eaf
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-27 04:37:08 -0300

    feat: Weather current component.

---
+++ src/app/public/components/weather-current/weather-current.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-current',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './weather-current.component.html',
+  styleUrl: './weather-current.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WeatherCurrentComponent { }
