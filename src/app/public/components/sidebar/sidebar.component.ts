7f62df0ae80d6a4b70c04e7bdae739f358a702b7
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-28 02:10:08 -0300

    refactor: The structure was simplified, improving readability and the function of the goBackBtn was added.

--- src/app/public/components/sidebar/sidebar.component.ts
+++ src/app/public/components/sidebar/sidebar.component.ts
@@ -1,24 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { SidebarService } from './../../services/sidebar.service';
-import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { ThemesComponent } from '../../../core/themes/themes.component';
-import { RouterModule } from '@angular/router';
-import { NotificationsComponent } from './config/notifications/notifications.component';
-import { UnitsComponent } from './config/units/units.component';
-import { IconsComponent } from './config/icons/icons.component';
-import { LangComponent } from './config/lang/lang.component';
-import { SearchbarComponent } from './searchbar/searchbar.component';
-import { FavsComponent } from './favs/favs.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { ModalService } from '../../services/modal.service';
-import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
+import { TranslateModule } from "@ngx-translate/core";
+
+import { InputTextModule } from "primeng/inputtext";
+import { SidebarModule } from "primeng/sidebar";
+import { ButtonModule } from "primeng/button";
+
+import { NotificationsComponent } from "./config/notifications/notifications.component";
+import { SidebarHeaderComponent } from "./sidebar-header/sidebar-header.component";
+import { SidebarFooterComponent } from "./sidebar-footer/sidebar-footer.component";
+import { ThemesComponent } from "../../../core/themes/themes.component";
+import { SearchbarComponent } from "./searchbar/searchbar.component";
+import { UnitsComponent } from "./config/units/units.component";
+import { IconsComponent } from "./config/icons/icons.component";
+import { LangComponent } from "./config/lang/lang.component";
+import { FavsComponent } from "./favs/favs.component";
+
+import { AuthService } from "../../../core/services/auth.service";
+import { SidebarService } from "./../../services/sidebar.service";
+import { ModalService } from "../../services/modal.service";
 
 @Component({
-  selector: 'app-sidebar',
+  selector: "app-sidebar",
   standalone: true,
   imports: [
     CommonModule,
@@ -26,6 +32,8 @@ import { AuthService } from '../../../core/services/auth.service';
     RouterModule,
     TranslateModule,
     SidebarModule,
+    SidebarHeaderComponent,
+    SidebarFooterComponent,
     ButtonModule,
     InputTextModule,
     SearchbarComponent,
@@ -36,8 +44,7 @@ import { AuthService } from '../../../core/services/auth.service';
     IconsComponent,
     LangComponent,
   ],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
+  templateUrl: "./sidebar.component.html",
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class SidebarComponent {
@@ -45,44 +52,44 @@ export class SidebarComponent {
   public modalService: ModalService = inject(ModalService);
   public authService: AuthService = inject(AuthService);
 
-  public currentMenu = 'home';
+  public currentMenu = "home";
 
   public showMenu: boolean = true;
   public configMenu: boolean = true;
   public btnConfig: any[] = [
     {
-      icon: 'pi pi-bell',
-      label: 'Notificaciones',
-      config: 'notifications',
-      disabled: 'true',
+      icon: "pi pi-bell",
+      label: "Notificaciones",
+      config: "notification",
+      disabled: "true",
     },
     {
-      icon: 'pi pi-compass',
-      label: 'Unidades',
-      config: 'units',
-      disabled: 'true',
+      icon: "pi pi-compass",
+      label: "Unidades",
+      config: "units",
+      disabled: "true",
     },
     {
-      icon: 'pi pi-moon',
-      config: 'themes',
-      label: 'Tema',
+      icon: "pi pi-moon",
+      config: "themes",
+      label: "Tema",
     },
     {
-      icon: 'pi pi-images',
-      label: 'Paquete de iconos',
-      config: 'icons',
-      disabled: 'true',
+      icon: "pi pi-images",
+      label: "Paquete de iconos",
+      config: "icons",
+      disabled: "true",
     },
     {
-      icon: 'pi pi-language',
-      label: 'Idioma',
-      config: 'lang',
+      icon: "pi pi-language",
+      label: "Idioma",
+      config: "lang",
     },
   ];
 
-  public sidebarCurrentRoute = 'sidebar';
+  public sidebarCurrentRoute = "sidebar";
 
-  public dynamicConfig: string = '';
+  public dynamicConfig: string = "";
 
   public showConfigClick() {
     this.showMenu = !this.showMenu;
@@ -98,7 +105,19 @@ export class SidebarComponent {
     this.modalService.openModal = true;
   }
 
-  public signOut(){
+  public signOut() {
     this.authService.signOut();
   }
+
+  public sidebarCurrent: string = "";
+  public sidebarNavigate(route: string) {
+    this.sidebarCurrent = route;
+  }
+
+  /**
+   * Primera pantalla Sidebar home => 'home'
+   * Segunda pantalla Configuracion => 'config'
+   *
+   *
+   */
 }
