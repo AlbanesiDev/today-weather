7d5d29fae3dea0b0af534c6768b4a1aefa6d99b8
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-04-03 00:57:38 -0300

    feat: The service handles icon list retrieval and stores user preferences.

--- src/app/core/services/weather-icons.service.ts
+++ src/app/core/services/weather-icons.service.ts
@@ -1,34 +1,100 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable, inject, signal } from "@angular/core";
-import { Storage, getDownloadURL, ref } from "@angular/fire/storage";
-import { listAll } from "@firebase/storage";
-import { firstValueFrom } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { LocalStorageService } from "./local-storage.service";
+import {
+  IconConfig,
+  IListIcons,
+  TWeatherIconsAnimations,
+  TWeatherIconsFolder,
+  TWeatherIconsType,
+} from "../interface/icon.interface";
+import { environment } from "../../../environments/environment";
 
+/**
+ * This service handles the retrieval of the icon list and stores user preferences
+ * regarding icons, allowing a switch between 'fill' and 'outline' icons and their
+ * respective non-animated versions. The stored values are used in a pipe to dynamically
+ * change the path of the icon to be displayed.
+ */
 @Injectable({
   providedIn: "root",
 })
 export class WeatherIconsService {
+  /**
+   * Injects the LocalStorageService dependency.
+   */
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
+
+  /**
+   * Injects the HttpClient dependency.
+   */
   private http: HttpClient = inject(HttpClient);
-  private storage: Storage = inject(Storage);
 
-  public iconsIsLoading = signal<boolean>(true);
-  public icons = signal<any[]>([]);
-  images: string[] = [];
+  /**
+   * Key for storing user icon preferences in local storage.
+   */
+  private iconsKeyStorage: string = environment.LOCAL_STORAGE.userIconPreferences;
 
-  public iconFolder = ["weather-fill", "weather-fill-static", "weather-outline", "weather-outline-static"];
+  /**
+   * URL to fetch the list of weather icons.
+   */
+  private iconsUrl: string = environment.WEATHER_ICON_LIST;
 
-  public getIcons(iconsFolder: string) {
-    const iconsRef = ref(this.storage, iconsFolder);
-    listAll(iconsRef)
-      .then(async (response) => {
-        console.log(response);
-        for (let image of response.items) {
-          const url = await getDownloadURL(image);
-          this.images.push(url);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  /**
+   * Stores the current icon type ('fill' or 'outline').
+   */
+  public iconsType = signal<TWeatherIconsType>("fill");
+
+  /**
+   * Stores whether the icon is animated ('on') or static ('off').
+   */
+  public iconsAnimations = signal<TWeatherIconsAnimations>("on");
+
+  /**
+   * Stores the folder path for the current icon type.
+   */
+  public iconsFolder = signal<TWeatherIconsFolder>("fill");
+
+  /**
+   * Configuration mapping for icon types and their corresponding folder and animation states.
+   */
+  private iconConfig: IconConfig = {
+    fill: { type: "fill", animation: "on", folder: "fill" },
+    outline: { type: "outline", animation: "on", folder: "outline" },
+    "fill-static": { type: "fill", animation: "off", folder: "fill-static" },
+    "outline-static": { type: "outline", animation: "off", folder: "outline-static" },
+  };
+
+  /**
+   * The constructor retrieves the stored icon type and sets the signals accordingly.
+   */
+  constructor() {
+    let iconTypeStorage = this.localStorageService.getItem(this.iconsKeyStorage) as keyof IconConfig;
+    const config = this.iconConfig[iconTypeStorage];
+
+    if (config) {
+      this.iconsType.set(config.type);
+      this.iconsAnimations.set(config.animation);
+      this.iconsFolder.set(config.folder);
+    }
+  }
+
+  /**
+   * Retrieves the list of icons as an Observable array of IListIcons.
+   * @returns An Observable of IListIcons array.
+   */
+  public getIcons(): Observable<IListIcons[]> {
+    return this.http.get<IListIcons[]>(this.iconsUrl);
+  }
+
+  /**
+   * Determines the icon folder path based on the current animation state and updates
+   * the local storage with the new icon folder value.
+   */
+  public getTypeIcons(): void {
+    const iconState = this.iconsAnimations() === "on" ? "" : "-static";
+    this.iconsFolder.set(`${this.iconsType()}${iconState}`);
+    this.localStorageService.setItem(this.iconsKeyStorage, this.iconsFolder());
   }
 }
