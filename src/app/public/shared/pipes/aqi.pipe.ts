eecc5bb2be5bc590018c5853c51653b9ef931676
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-04-10 23:45:58 -0300

    feat: Aqi Pipe.

---
+++ src/app/public/shared/pipes/aqi.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, type PipeTransform } from "@angular/core";
+/**
+ * Pipe that transforms air quality index (AQI) pollutant codes into their respective chemical symbols.
+ */
+@Pipe({
+  name: "aqiPipe",
+  standalone: true,
+})
+export class AqiPipe implements PipeTransform {
+  /**
+   * Transforms the input pollutant code to its chemical symbol.
+   * @param value The pollutant code as a string.
+   * @returns The chemical symbol for the given pollutant code or the original value if no match is found.
+   */
+  transform(value: unknown): string | unknown {
+    switch (value) {
+      case "co":
+        return "CO";
+      case "nh3":
+        return "NH";
+      case "no":
+        return "NO";
+      case "no2":
+        return "NO₂";
+      case "o3":
+        return "O₃";
+      case "pm10":
+        return "PM 10";
+      case "pm2_5":
+        return "PM 2,5";
+      case "so2":
+        return "SO₂";
+    }
+    return value;
+  }
+}
