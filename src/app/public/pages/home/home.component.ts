4bf5121272c137ccb5aff7561ef305b432051041
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-04-23 14:38:30 -0300

    update: Home component

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  inject,
+  signal,
+} from "@angular/core";
 import { HeaderComponent } from "../../components/header/header.component";
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { WeatherCurrentComponent } from "../../components/weather-current/weather-current.component";
 import { AuthComponent } from "../../components/auth/auth.component";
 import { WeatherCurrentDetailComponent } from "../../components/weather-current-detail/weather-current-detail.component";
-import { WeatherForecastDailyComponent } from "../../components/weather-forecast-daily/weather-forecast-daily.component";
 import { FeaturesService } from "./../../../core/services/features.service";
-import { WeatherForecastHourlyComponent } from "../../components/weather-forecast-hourly/weather-forecast-hourly.component";
 import { WeatherAqiComponent } from "../../components/weather-aqi/weather-aqi.component";
+import { Subject, takeUntil } from "rxjs";
+import { LoaderService } from "../../../core/services/loader.service";
+import { WeatherForecastComponent } from "../../components/weather-forecast/weather-forecast.component";
+import { GeolocationService } from "../../../core/services/geolocation.service";
+import { initializeApp } from "@angular/fire/app";
+import { InitializeAppService } from "../../../core/services/initialize-app.service";
+import { TranslateModule } from "@ngx-translate/core";
 
 /**
  * The HomeComponent is responsible for rendering the main home view.
@@ -21,14 +34,14 @@ import { WeatherAqiComponent } from "../../components/weather-aqi/weather-aqi.co
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
     HeaderComponent,
     SidebarComponent,
     AuthComponent,
     WeatherCurrentComponent,
-    WeatherForecastHourlyComponent,
     WeatherCurrentDetailComponent,
-    WeatherForecastDailyComponent,
     WeatherAqiComponent,
+    WeatherForecastComponent,
   ],
   template: `
     <app-sidebar />
@@ -36,24 +49,86 @@ import { WeatherAqiComponent } from "../../components/weather-aqi/weather-aqi.co
     <div class="container">
       <app-header />
       <div class="flex flex-column gap-6">
-        <app-weather-current [class]="'flex-order-' + getPosition(0)" />
-        <app-weather-forecast-hourly [class]="'flex-order-' + getPosition(1)" />
-        <app-weather-current-detail [class]="'flex-order-' + getPosition(2)" />
-        <app-weather-forecast-daily [class]="'flex-order-' + getPosition(3)" />
-        <app-weather-aqi [class]="'flex-order-' + getPosition(4)" />
+        @if (loaderService.isErrorSig()) {
+          <div
+            class="flex justify-content-center align-items-center border-1 border-primary border-round h-25rem"
+          >
+            <p class="text-4xl text-red-500">
+              {{ loaderService.errorMessage() }}
+            </p>
+          </div>
+        } @else {
+          <app-weather-current
+            [data]="weatherDataSig()"
+            [geoData]="geolocationDataSig()"
+            [class]="'flex-order-' + getPosition(0)"
+          />
+          <app-weather-forecast
+            forecastType="daily"
+            [class]="'flex-order-' + getPosition(3)"
+            [dataInput]="weatherDataSig().daily"
+          />
+          <app-weather-forecast
+            forecastType="hourly"
+            [class]="'flex-order-' + getPosition(1)"
+            [dataInput]="weatherDataSig().hourly"
+          />
+          <app-weather-current-detail
+            [dataInput]="weatherDataSig().current"
+            [class]="'flex-order-' + getPosition(2)"
+          />
+          <app-weather-aqi
+            [dataInput]="weatherDataSig().aqi"
+            [class]="'flex-order-' + getPosition(4)"
+          />
+        }
       </div>
-      <footer class="flex justify-content-center align-items-center p-7 h-3rem">
+      <footer class="flex justify-content-center align-items-center my-7 h-3rem">
         <div class="text-xl font-medium">
-          Copyright 2024 |
-          <a href="https://github.com/AlbanesiDev" class="p-0 text-primary no-underline hover:underline">AlbanesiDev</a>
+          {{ "footer.copyright" | translate }} 2024 |
+          <a
+            class="p-0 text-primary no-underline hover:underline"
+            href="https://github.com/AlbanesiDev"
+            target="_blank"
+            >AlbanesiDev</a
+          >
         </div>
       </footer>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  public loaderService: LoaderService = inject(LoaderService);
   public featuresService: FeaturesService = inject(FeaturesService);
+  private initializeAppService: InitializeAppService = inject(InitializeAppService);
+  private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+  private unsubscribe$ = new Subject<boolean>();
+  public weatherDataSig = signal<any>([]);
+  public geolocationDataSig = signal<any>([]);
+  public errorMessage: undefined;
+
+  ngOnInit(): void {
+    this.initializeAppService
+      .initWeather()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res) => {
+          this.weatherDataSig.set(res.weather);
+          this.geolocationDataSig.set(res.geo);
+        },
+        error: (err) => {
+          this.loaderService.errorMessage.set(err);
+        },
+      });
+    this.cdr.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
+  }
 
   /**
    * Retrieves the current position of a feature by its ID.
